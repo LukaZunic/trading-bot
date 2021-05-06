c15f3bd6fcba94313fb42bf0d480f4e7957730c8
LukaZunic <lzunic99@gmail.com>
2021-05-06 16:32:33 +0200

    added Dashboard subcomponents

--- Exchange/src/app/app.module.ts
+++ Exchange/src/app/app.module.ts
@@ -10,18 +10,37 @@ import { OrderComponent } from './components/order/order.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+/* ANGULAR MATERIAL IMPORTS */
+import {MatIconModule} from '@angular/material/icon';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatButtonModule} from '@angular/material/button';
+import { MatSliderModule } from '@angular/material/slider';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { MarketComponent } from './components/market/market.component';
+import { OrdersComponent } from './components/orders/orders.component';
+import { GraphComponent } from './components/graph/graph.component';
+
+
 @NgModule({
   declarations: [
     AppComponent,
     WalletComponent,
     OrderComponent,
-    NavbarComponent
+    NavbarComponent,
+    DashboardComponent,
+    MarketComponent,
+    OrdersComponent,
+    GraphComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatIconModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSliderModule
   ],
   providers: [
     HttpClientModule
