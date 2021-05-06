a38535be6107a6ad393e887fe7e260908e2313a7
LukaZunic <lzunic99@gmail.com>
2021-05-06 17:38:03 +0200

    components added

---
+++ Exchange/src/app/components/orders/orders.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import {MarketService} from '../../services/market.service';
+import {Observable} from 'rxjs';
+import {map, tap} from 'rxjs/operators';
+
+@Component({
+  selector: 'app-orders',
+  templateUrl: './orders.component.html',
+  styleUrls: ['./orders.component.scss']
+})
+export class OrdersComponent implements OnInit {
+
+  orders$: Observable<any>;
+
+  constructor(public market: MarketService) { }
+
+  ngOnInit(): void {
+    this.orders$ = this.market.orderData$.pipe(
+      tap(data => console.log(data.data)),
+      map(data => data.data)
+    );
+  }
+
+}
