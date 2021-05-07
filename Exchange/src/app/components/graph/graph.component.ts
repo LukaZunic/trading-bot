0cbb281102d474ef86be92ccef2a1cf47a312c0d
LukaZunic <lzunic99@gmail.com>
2021-05-07 23:10:09 +0200

    work on bot initialization

--- Exchange/src/app/components/graph/graph.component.ts
+++ Exchange/src/app/components/graph/graph.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {MarketService} from '../../services/market.service';
 
 @Component({
   selector: 'app-graph',
@@ -7,9 +8,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GraphComponent implements OnInit {
 
-  constructor() { }
+  constructor(public market: MarketService) { }
 
   ngOnInit(): void {
+
   }
 
 }
--- Exchange/src/app/services/market.service.ts
+++ Exchange/src/app/services/market.service.ts
@@ -17,14 +17,20 @@ export class MarketService {
     this.wallet$ = this.http.get('http://localhost:3014/api/getAllWallet');
   }
 
-
   initializeBot(crypto: string, method: string, balance: number): Observable<any> {
 
     // this.http.post(`http://localhost:3014/api/${bot}/script`, );
-    return this.http.post<any>(`http://localhost:3014/api/createWallet`, {
+    // return this.http.post<any>(`http://localhost:3014/api/createWallet`, {
+    //     name: crypto,
+    //     balance,
+    //     method
+    // });
+
+    // const date = new Date().toISOString().slice(0, 10);
+
+    return this.http.post(`http://localhost:3014/api/${method}/start`, {
         name: crypto,
-        balance,
-        method
+        start_date: '2020-01-01'
     });
 
   }
